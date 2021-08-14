bdf07414188ca6e8b85a02691a9457daf5e4498b
KiditsGood <sadol2001@mail.ru>
2021-08-15 02:46:00 +0300

    123

---
+++ js/scroll.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".nav-menu, .learn").on("click","a", function (event) {
+        event.preventDefault();
+        let id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
